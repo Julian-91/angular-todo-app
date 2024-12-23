ace5b479d4966b624e4c9f0dab985d66ef55ca58
Julian Boot <julian_boot@hotmail.com>
2024-12-23 14:21:29 +0100

    Updated config to use snapshot file names without platform

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -14,6 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   snapshotDir: './tests/snapshots',
+  snapshotPathTemplate: '{snapshotDir}/{testFilePath}/{arg}{ext}',
   expect: {
     toHaveScreenshot: { maxDiffPixels: 100 },
   },
